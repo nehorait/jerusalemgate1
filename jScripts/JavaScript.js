6284fa4b1867b5bead8f83232b88a53937cb3939
nehorait <mgrxaby@gmail.com>
2025-06-27 13:36:06 +0300

    dsa

--- jScripts/JavaScript.js
+++ jScripts/JavaScript.js
@@ -1,4 +1,12 @@
-﻿function radioImageChooser(){
+﻿function launch(){
+    var image = document.getElementById("filterMainImage").value;
+    console.log(image);
+}
+
+
+function radioImageChooser(){
+    var currectRadioChoice = document.getElementByType("radio").value;
+    var image = document.getElementById("filterMainImage").value;
     
 }
 
