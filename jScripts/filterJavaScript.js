a2560171c5f019280f8812f20f65f6ed740bd7cd
nehorait <mgrxaby@gmail.com>
2025-06-29 01:58:01 +0300

    dsa

--- jScripts/filterJavaScript.js
+++ jScripts/filterJavaScript.js
@@ -38,24 +38,24 @@ function activeRadioChoice(){
 }
 function activeCheckboxChoices(){
     userCheckboxChoices = [];
-    const checkboxMostInterested = document.getElementsByName("mostInterested");{
+    const checkboxMostInterested = document.getElementsByName("mostInterested");
         for (let i=0; i < checkboxMostInterested.length; i++){
             if (checkboxMostInterested[i].checked === true){
                 userCheckboxChoices[i]= i+1;
             }
+            console.log(userCheckboxChoices);
         }
         if (userCheckboxChoices[0] === 1){
-            console.log(userCheckboxChoices[0]);
+            
         }
         if (userCheckboxChoices[1] === 2){
-            console.log(userCheckboxChoices[1]);
+            
         }
         if (userCheckboxChoices[2] === 3){
-            console.log(userCheckboxChoices[2]);
+            
         }
         if (userCheckboxChoices[3] === 4){
-            console.log(userCheckboxChoices[3]);
+            
         }
         console.log(userCheckboxChoices);
-    }
 }
