6eef4012c458949ae162c22f820bea342d19b376
nehorait <mgrxaby@gmail.com>
2025-06-29 04:44:58 +0300

    dsa

--- jScripts/filterJavaScript.js
+++ jScripts/filterJavaScript.js
@@ -1,26 +1,54 @@
 ﻿let userFirstName = "";
 let userRadioChoice = 0;
 let userCheckboxChoices = 0;
-function match(){
-    firstName();
+let isUserFirstNameValid = false;
+let isUserRadioChoiceValid = false;
+let isUserCheckBoxChoicesValid = false;
+function clear(){
+    userFirstName = "";
+    userRadioChoice = 0;
+    userCheckboxChoices = 0;
+    isUserFirstNameValid = false;
+    isUserRadioChoiceValid = false;
+    isUserCheckBoxChoicesValid = false;
 }
-function firstName(){
-    const textFirstName = document.getElementById("firstName").value;
-    if (textFirstName.length >= 2){
+//function match(){
+    //    firstName();
+//}
+ function firstName() {
+    const hebrewRegex = /^[\u0590-\u05FF\s.,'"-]+$/;
+    const textFirstName = document.getElementById("firstName").value.trim();
+    if (textFirstName.length >= 2 && hebrewRegex.test(textFirstName)) {
         userFirstName = textFirstName;
-        console.log(userFirstName);
-        activeRadioChoice();
+        isUserFirstNameValid = true;
+    //    if (userRadioChoice === 0 && userCheckboxChoices === 0) {
+         //   alert("בחר/י אופציה אחת מהכתפורים העגולים ובאחת מהכפתורים המרובעים לפחות");
+       // }else{
+       //     if (userRadioChoice === 0 && userCheckboxChoices !== 0) {
+       //         alert("בחר/י אופציה אחת מהכתפורים העגולים");
+       //     }else{
+         //       if (userRadioChoice !== 0 && userCheckboxChoices === 0) {
+           //         alert("בחר/י אופציה אחת מהכפתורים המרובעים לפחות");
+///
+           //     }else{
+               //     alert("כל הכבוד!")
+         //       }
+      //      }
+    //    }
     }else{
-        alert("הקלד בבקשה שם פרטי או כינוי בעברית בלבד שגדול או שווה ל 2 תווים")
+        alert("הקלד/י בבקשה שם פרטי או כינוי בעברית בלבד שגדול או שווה ל 2 תווים");
+        isUserFirstNameValid = false;
     }
+     checkFormValidity()
 }
+
 function activeRadioChoice(){
     const mainImage = document.getElementById("filterMainImage");
     const radioTripType = document.getElementsByName("tripType");
     for (let i = 0; i < radioTripType.length; i++) {
         if (radioTripType[i].checked === true){
             userRadioChoice = i+1;
-            console.log(userRadioChoice);
+            isUserRadioChoiceValid = true;
             break;
         }
     }
@@ -32,30 +60,75 @@ function activeRadioChoice(){
         mainImage.src = "images/filterform/mainimage/shoppinginstyle.jpeg";
     }else if(userRadioChoice === 4){
         mainImage.src = "images/filterform/mainimage/eternallycurious.jpeg";
-}
+    }
     mainImage.style.opacity = "100%";
-    activeCheckboxChoices();
+    checkFormValidity()
 }
 function activeCheckboxChoices(){
     userCheckboxChoices = [];
+    const mainImagesSpecialAttractions = document.getElementsByClassName("filterMainImagesSpecialAttractions");
+    const mainImagesHistoricalSites = document.getElementsByClassName("filterMainImagesHistoricalSites");
+    const mainImagesMuseums = document.getElementsByClassName("filterMainImagesMuseums");
+    const mainImagesViewpoints = document.getElementsByClassName("filterMainImagesViewpoints");
     const checkboxMostInterested = document.getElementsByName("mostInterested");
         for (let i=0; i < checkboxMostInterested.length; i++){
             if (checkboxMostInterested[i].checked === true){
                 userCheckboxChoices[i]= i+1;
             }
-            console.log(userCheckboxChoices);
         }
-        if (userCheckboxChoices[0] === 1){
-            
+    if (userCheckboxChoices[0] === 1){
+        for (let i=0; i < mainImagesSpecialAttractions.length; i++){
+            mainImagesSpecialAttractions[i].style.opacity = "100%";
         }
+        }else{
+        for (let i=0; i < mainImagesSpecialAttractions.length; i++){
+            mainImagesSpecialAttractions[i].style.opacity = "50%";
+        }
+    }
         if (userCheckboxChoices[1] === 2){
-            
+            for (let i=0; i < mainImagesHistoricalSites.length; i++){
+                mainImagesHistoricalSites[i].style.opacity = "100%";
+            }
+        }else{
+            for (let i=0; i < mainImagesHistoricalSites.length; i++){
+                mainImagesHistoricalSites[i].style.opacity = "50%";
+            }
         }
         if (userCheckboxChoices[2] === 3){
-            
+            for (let i=0; i < mainImagesMuseums.length; i++){
+                mainImagesMuseums[i].style.opacity = "100%";
+            }
+        }else{
+            for (let i=0; i < mainImagesMuseums.length; i++){
+                mainImagesMuseums[i].style.opacity = "50%";
+            }
         }
         if (userCheckboxChoices[3] === 4){
-            
+            for (let i=0; i < mainImagesViewpoints.length; i++){
+                mainImagesViewpoints[i].style.opacity = "100%";
+            }
+        }else{
+            for (let i=0; i < mainImagesViewpoints.length; i++){
+                mainImagesViewpoints[i].style.opacity = "50%";
+            } 
+        }    
+        if (userCheckboxChoices[0] === 1 || userCheckboxChoices[1] === 2 || userCheckboxChoices[2] === 3 || userCheckboxChoices[3] === 4){
+            isUserCheckBoxChoicesValid = true;
+        }else{
+            isUserCheckBoxChoicesValid = false;
         }
-        console.log(userCheckboxChoices);
+    checkFormValidity()
 }
+
+function checkFormValidity(){
+    const buttonMatch = document.getElementById("match");
+    if (isUserFirstNameValid === true && isUserRadioChoiceValid === true && isUserCheckBoxChoicesValid === true ){
+        console.log("enabled");
+        buttonMatch.disabled = false;
+    }else{
+        console.log ("disabled");
+        buttonMatch.disabled  = true;
+    }
+}
+
+
